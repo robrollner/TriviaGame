fd4419aef110d4974b032c63d61dc4b276f47785
Robert Rollner <robrollner@gmail.com>
2017-08-18 21:33:54 -0600

    adding first question update

--- assets/Javascript/app.js
+++ assets/Javascript/app.js
@@ -1,29 +1,62 @@
-window.onload = function() {
-    $("#start").on("click", timer.start);
-};
+    var questionBoxQuestions = [{
+        question: "What is Mr. Burns First Name?",
+        choices: ["I don't know...", "Barney", "Waylon", "Charles"],
+        correctAnswer: 3
+    }, {
+        question: "What college did Dr. Nick Riviera graduate from?",
+        choices: ["Shelbyville state", "Hollywood Upstairs Medical College", "Springfield U", "Cornell"],
+        correctAnswer: 1
+    }, {
+        question: "What smells like a steak and seats 35?",
+        choices: ["Canyonero", "Rawhidanator", "Homer Simpson", "Springfield elementary school bus"],
+        correctAnswer: 0
+    }, {
+        question: "Who voiced Maggie Simpson?",
+        choices: ["Yeardley Smith", "Elizabeth Hurley", "Elizabeth Taylor", "Julie Kavner"],
+        correctAnswer: 2
+    }];
 
-var number = 2;
-var intervalId;
+    console.log(questionBoxQuestions[0].question);
+    console.log(questionBoxQuestions[0].choices[0]);
 
-$('#stop').on('click', stop);
-$('#start').on('click', run);
+    var currentQuestion = 0;
+    var correctAnswers = 0;
+    var incorrectAnswers = 0;
+    var quizOver = false;
 
-function run() {
-    intervalId = setInterval(decrement, 1000);
-    // 	const myQuestions = [{
-    // 		question: "What is Mr. Burns' first name?"
-    // 		answers: {
-    // 			a:"Homer",
-    // 			b:"Barney",
-    // 			c:"Carl",
-    // 			d:"Charles"
-    // 		},
-    // 		correctAnswer: "c"
-    // 	},
-    // 	{
-    // 		question: ""
-    // 	}]
-     }
+
+
+    window.onload = function() {
+        $("#start").on("click", timer.start);
+        // $('#questionBox').empty();
+        // $('.btn-info').empty();
+        // for (var i = 0; i < questionBoxQuestions.length; i++) {
+        //     $('#questionBox').html(questionBoxQuestions.question[i].question);
+
+
+
+
+    };
+
+    var number = 30; //change to 30
+    var intervalId;
+
+
+
+    $('#stop').on('click', stop);
+    $('#start').on('click', run);
+
+
+    function run() {
+        intervalId = setInterval(decrement, 1000);
+        $('#questionBox').empty();
+        $('.btn-info').empty();
+        $('#questionBox').prepend('<h2>' + questionBoxQuestions[0].question + '</h2>');
+        for (var i = 0; i < questionBoxQuestions.length; i++) {
+            $('.btn-info').append('<h4>' + questionBoxQuestions[0].choices[i] + '</h4>');
+        }
+
+    }
 
     function decrement() {
         number--;
@@ -31,58 +64,16 @@ function run() {
         if (number === 0) {
             stop();
             $('#questionBox').html("I said HA HA!!!!!");
-            $('#questionBox').slideUp(300).delay(800).fadeIn(400).prepend('<img id="NelsonGif" src="https://raw.githubusercontent.com/robrollner/TriviaGame/master/assets/images/NelsonGif.gif" />');
+            $('#questionBox').slideUp(1000).delay(300).fadeIn(1000).prepend('<img class="img-responsive" id="resultGif" src="https://raw.githubusercontent.com/robrollner/TriviaGame/master/assets/images/NelsonGif.gif" />');
             //add to incorrect score
 
-        }
+        } //else if (correct answer scenario)
     }
 
     function stop() {
         clearInterval(intervalId);
         var number = 30; //work on a reset...
         $('#questionBox').html("I hate it...")
+        $('#questionBox').slideUp(1000).delay(300).fadeIn(1000).prepend('<img class="img-responsive" id="resultGif" src="https://raw.githubusercontent.com/robrollner/TriviaGame/master/assets/images/BarneyGif.gif" />');
+
     };
-
-
-
-
-
-
-
-    // var intervalId;
-    // var clockRunning = false;
-
-    // var timer = {
-    //     time: 0,
-    //     reset: function() {
-    //         $('#timer').html(':30');
-    //     },
-    //     start: function() {
-    //         if (!clockRunning) {
-    //             intervalId = setInterval(timer.count, 1000);
-    //             clockRunning = true;
-    //         }
-    //     },
-
-    //     count: function() {
-    //         timer.time++;
-    //         var converted = timer.timeConverter(timer.time);
-    //         console.log(converted);
-    //         $('#timer').html(converted);
-    //     },
-
-    //     timeConverter: function(t) {
-    //         var minutes = Math.floor(t / 60);
-    //         var seconds = t - (minutes * 60);
-    //         if (seconds < 10) {
-    //             seconds = "0" + seconds;
-    //         }
-    //         if (minutes === 0) {
-    //             minutes = "00";
-    //         } else if (minutes < 10) {
-    //             minutes = "0" + minutes;
-    //         }
-    //         return minutes + ":" + seconds;
-    //     }
-
-    // };
